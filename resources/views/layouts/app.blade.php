c31bb11aad94a58c395e572807203c80ff1af674
pergeld <pergeldaniel@gmail.com>
2021-10-19 18:43:17 +0200

    inputs - rich-text editor

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,6 +18,9 @@
         <!-- Pikaday -->
         <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/pikaday/css/pikaday.css">
 
+        <!-- Trix (texteditor) -->
+        <link rel="stylesheet" type="text/css" href="https://unpkg.com/trix@1.2.3/dist/trix.css">
+
         <!-- Livewire -->
         @livewireStyles
 
@@ -34,6 +37,8 @@
             <x-notification />
         </div>
         @livewireScripts
+        <script src="https://unpkg.com/moment"></script>
         <script src="https://cdn.jsdelivr.net/npm/pikaday/pikaday.js"></script>
+        <script src="https://unpkg.com/trix@1.2.3/dist/trix.js"></script>
     </body>
 </html>
--- resources/views/livewire/profile.blade.php
+++ resources/views/livewire/profile.blade.php
@@ -14,22 +14,10 @@
             </x-input.group>
 
             <x-input.group label="About" for="about" :error="$errors->first('about')" help-text="Write a few sentances about yourself.">
-                <x-input.textarea wire:model="about" id="about" />
+                <x-input.rich-text wire:model.lazy="about" id="about" :initial-value="$about" />
             </x-input.group>
 
-            <div class="mt-6 sm:mt-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
-                <label for="about" class="block text-sm font-medium leading-5 text-gray-700 sm:mt-px sm:pt-2">
-                    About
-                </label>
-
-                <div class="mt-1 sm:mt-0 sm:col-span-2">
-                    <div class="max-w-lg flex rounded-md shadow-sm">
-                        <textarea wire:model="about" id="about" rows="3" class="form-textarea block w-full transition duration-150 ease-in-out sm:text-sm sm:leading-5"></textarea>
-                    </div>
-                    @error('about') <div class="mt-1 text-red-500 text-sm">{{ $message }}</div> @enderror
-                    <p class="mt-2 text-sm text-gray-500">Write a few sentences about yourself.</p>
-                </div>
-            </div>
+            
 
             <div class="mt-6 sm:mt-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:items-center sm:border-t sm:border-gray-200 sm:pt-5">
                 <label for="photo" class="block text-sm leading-5 font-medium text-gray-700">
