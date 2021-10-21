23f5067200586a0d0e732843e9d7881fc7c882b3
pergeld <pergeldaniel@gmail.com>
2021-10-21 14:56:24 +0200

    file uploads - basic upload

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Storage;
 
 class User extends Authenticatable
 {
@@ -21,6 +22,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'avatar',
     ];
 
     /**
@@ -41,4 +43,11 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function avatarUrl()
+    {
+        return $this->avatar
+            ? Storage::disk('avatars')->url($this->avatar)
+            : 'https://www.gravatar.com/avatar/'.md5(strtolower(trim($this->email)));
+    }
 }
--- resources/views/livewire/profile.blade.php
+++ resources/views/livewire/profile.blade.php
@@ -19,27 +19,21 @@
 
             
 
-            <div class="mt-6 sm:mt-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:items-center sm:border-t sm:border-gray-200 sm:pt-5">
-                <label for="photo" class="block text-sm leading-5 font-medium text-gray-700">
-                    Photo
-                </label>
+            <x-input.group label="Photo" for="photo" :error="$errors->first('newAvatar')">
+                <div class="flex items-center">
+                    <span class="h-12 w-12 rounded-full overflow-hidden bg-gray-100">
+                        {{-- <img src="{{ auth()->user()->avatarUrl() }}" alt="Profile Photo"> --}}
+                    </span>
 
-                <div class="mt-2 sm:mt-0 sm:col-span-2">
-                    <div class="flex items-center">
-                        <span class="h-12 w-12 rounded-full overflow-hidden bg-gray-100">
-                            <svg class="h-full w-full text-gray-300" fill="currentColor" viewBox="0 0 24 24">
-                                <path d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z" />
-                            </svg>
-                        </span>
-
-                        <span class="ml-5 rounded-md shadow-sm">
-                            <button type="button" class="py-2 px-3 border border-gray-300 rounded-md text-sm leading-4 font-medium text-gray-700 hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-50 active:text-gray-800 transition duration-150 ease-in-out">
-                                Change
-                            </button>
-                        </span>
-                    </div>
+                    <span class="ml-5 rounded-md shadow-sm">
+                        <input type="file" wire:model="newAvatar">
+                        <!-- <button type="button" class="py-2 px-3 border border-gray-300 rounded-md text-sm leading-4 font-medium text-gray-700 hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-50 active:text-gray-800 transition duration-150 ease-in-out">
+                            Change
+                        </button> -->
+                    </span>
                 </div>
-            </div>
+            </x-input.group>
+
         </div>
 
         <div class="mt-8 border-t border-gray-200 pt-5">
